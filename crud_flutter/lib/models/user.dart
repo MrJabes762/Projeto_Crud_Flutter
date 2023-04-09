08c5cbaae4972e77d6862d757bb7f9121a9dc9d4
MrJabes762 <118413268+MrJabes762@users.noreply.github.com>
2023-04-09 19:19:33 -0300

    Adição de dados Blocados de cada Usuário (#3)
    * configurando_ambiente
    
    * add_Readme
    
    * criação_de_dados_blocados_de_usuários

---
+++ crud_flutter/lib/models/user.dart
@@ -0,0 +1,14 @@
+
+class User {
+  final String id;
+  final String name;
+  final String email;
+  final String avatarUrl;
+
+  const User({
+    this.id = 'NULL',
+    this.name = 'NULL',
+    this.email = 'NULL',
+    this.avatarUrl = 'NULL',
+  });
+}
