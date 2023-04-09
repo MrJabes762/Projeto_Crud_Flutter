f4635753aea11db794a0fc6d96a11bdb3dcaccc8
MrJabes762 <jabessilva31@gmail.com>
2023-04-09 19:15:55 -0300

    criação_de_dados_blocados_de_usuários

---
+++ crud_flutter/lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:crud_flutter/models/user.dart';
+// ignore: constant_identifier_names
+const DUMMY_USERS = {
+  '1': User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png',
+  ),
+  '2': User(
+    id: '2',
+    name: 'Rafael',
+    email: 'rafael@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/12/13/16/17/dancer-1904467_960_720.png',
+  ),
+  '3': User(
+    id: '3',
+    name: 'Fernanda',
+    email: 'Fernanda@gmae.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2021/05/10/08/00/woman-6242836_960_720.png',
+  ),
+  '4': User(
+    id: '4',
+    name: 'Carlos',
+    email: 'Carlos@homstl.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2018/08/28/13/29/avatar-3637561_960_720.png',
+  ), 
+};
