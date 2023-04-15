e9da85cdb971a7e741e636ebae089979dbd7f740
MrJabes762 <118413268+MrJabes762@users.noreply.github.com>
2023-04-15 14:55:48 -0300

    Alterações em Métodos  (#7)
    * Modificações no arquivo de usuarios
    
    * Alterações em Métodos

--- crud_flutter/lib/provider/users.dart
+++ crud_flutter/lib/provider/users.dart
@@ -12,4 +12,7 @@ class Users with ChangeNotifier {
   int get count {
     return _items.length;
   }
+  User byIndex(int i) {
+    return _items.values.elementAt(i);
+  }
 }
--- crud_flutter/lib/views/user_list.dart
+++ crud_flutter/lib/views/user_list.dart
@@ -1,12 +1,13 @@
-import 'package:crud_flutter/data/dummy_users.dart';
 import 'package:flutter/material.dart';
-import '../components/user_tile.dart';
+import 'package:provider/provider.dart';
+import 'package:crud_flutter/components/user_tile.dart';
+import 'package:crud_flutter/provider/users.dart';
 
 class UserList extends StatelessWidget {
   const UserList({super.key});
   @override
   Widget build(BuildContext context) {
-    final users = {...DUMMY_USERS};
+    final Users users = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Lista de Usuários'),
@@ -18,8 +19,8 @@ class UserList extends StatelessWidget {
         ],
       ),
       body: ListView.builder(
-        itemCount: users.length,
-        itemBuilder: (ctx, i) => UserTile(users.values.elementAt(i)),
+        itemCount: users.count,
+        itemBuilder: (ctx, i) => UserTile(users.byIndex(i)),
       ),
     );
   }
