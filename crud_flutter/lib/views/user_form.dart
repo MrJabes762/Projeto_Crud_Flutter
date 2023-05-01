c309cd3c46666a25f59c219df764e84d6c72dcd7
MrJabes762 <jabessilva31@gmail.com>
2023-05-01 00:39:30 -0300

    Add Formulario de Usuario

--- crud_flutter/lib/views/user_form.dart
+++ crud_flutter/lib/views/user_form.dart
@@ -9,6 +9,32 @@ class Userform extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulario de Usuario'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(15),
+        child: Form(
+          child: Column (
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration (labelText: 'Nome'),
+              ),
+              TextFormField(
+                decoration: InputDecoration (labelText: 'Email'),
+              ),
+              TextFormField(
+                decoration: InputDecoration (labelText: 'URL do Avatar'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
