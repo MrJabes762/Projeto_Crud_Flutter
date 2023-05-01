139ca2c6a98fd44d951133b09a4e2f4a476345d9
Jabes Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2023-05-01 01:10:24 -0300

    Add_Validação (#11)
    * Modificações no arquivo de usuarios
    
    * Alterações em Métodos
    
    * Método put com inclusão e alteração do usuário
    
    * Finalização dos metodos + navegação animada
    
    * Add Formulario de Usuario
    
    * Add_Validação

--- crud_flutter/lib/views/user_form.dart
+++ crud_flutter/lib/views/user_form.dart
@@ -4,6 +4,8 @@ import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
 
 class Userform extends StatelessWidget {
+  final _form = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +16,12 @@ class Userform extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.save),
             onPressed: () {
+              final isValid = _form.currentState!.validate();
+
+              if (isValid) {
+                _form.currentState!.save();
+                Navigator.of(context).pop();
+              }
               Navigator.of(context).pop();
             },
           ),
@@ -22,6 +30,30 @@ class Userform extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.all(15),
         child: Form(
+          key: _form,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Nome'),
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'Nome inválido';
+                  }
+                  if (value.trim().length < 3){
+                    return 'Nome muito pequeno. No minimo 3 letras';
+                  }
+                  
+                  return null;
+                },
+                onSaved: (value) {
+                  print(value);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'URL do Avatar'),
           child: Column (
             children: <Widget>[
               TextFormField(
