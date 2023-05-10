ad0ee717ce01793fe7dca8bf920f120683e40ae4
Jabes Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2023-05-10 18:12:24 -0300

    Finalizando Projeto (#15)
    * Modificações no arquivo de usuarios
    
    * Alterações em Métodos
    
    * Método put com inclusão e alteração do usuário
    
    * Finalização dos metodos + navegação animada
    
    * Add Formulario de Usuario
    
    * Add_Validação
    
    * funcinalidade de inclusão
    
    * finalizando alteraçao de usuario
    
    * Projeto finalizado

--- crud_flutter/lib/components/user_tile.dart
+++ crud_flutter/lib/components/user_tile.dart
@@ -1,6 +1,7 @@
 import 'package:crud_flutter/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:crud_flutter/models/user.dart';
+import 'package:provider/provider.dart';
 
 class UserTile extends StatelessWidget {
   final User user;
@@ -30,7 +31,30 @@ class UserTile extends StatelessWidget {
               icon: Icon(Icon.edit),
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                showDialog(
+                  context: context, 
+                  builder: (ctx) => AlertDialog(
+                  title: Text ('Excluir Usuário'),
+                  content: Text('Tem certeza ???'),
+                  actions: <Widget> [ 
+                    FlatButton (
+                      child: Text('Não'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    )
+                    FlatButton (
+                      child: Text('Sim'),
+                      onPressed: () {
+                        Provider.of<Users>(context, listen: false).remove(user);
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                  ),
+                );
+              },
               color: Color.red,
               icon: Icon(Icon.delete),
             ),
